7320c830533ea5707d2feb2e92289fe01b4655a4
Christian Marques <145080782+christian-marques@users.noreply.github.com>
2024-04-23 22:27:30 -0300

    Organização inicial do salvamento das fotos
    A foto está sendo salva na pasta "fotos" da raiz, com o nome da mesma sendo o instante de tempo em que foi tirada.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,36 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 const app = express();
 app.use(express.static('public'), express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
 const port = 3030;
+const folderPath = 'fotos';
+
+function checkFolderPath(){
+    const fotosPath = path.join(__dirname, '..', folderPath);
+    if (!fs.existsSync(fotosPath)) {
+        fs.mkdirSync(fotosPath);
+    }
+}
 
 app.post('/save-image', (req, res) => {
-    const base64Data = req.body.image.replace(/^data:image\/png;base64,/, "");
-    fs.writeFile('image.png', base64Data, 'base64', err => {
+    checkFolderPath();
+
+    const data = req.body.image;
+    const base64Data = data.replace(/^data:image\/png;base64,/, "");
+    const datetime = new Date().toISOString().replace(/:/g, '-');
+    const filename = `photo-${datetime}.png`;
+    const filePath = path.join(__dirname, '..', folderPath, filename);
+
+    fs.writeFile(filePath, base64Data, 'base64', err => {
         if (err) {
             console.error(err);
-            return res.status(500).send('Error saving image');
+            return res.status(500).send(err);
         }
-        res.send({ message: 'Image saved successfully' });
+        console.log('Imagem salva: ', filePath);
+        res.send({ message: 'Imagem salva com sucesso: ', filename});
     });
 });
 
